742b9aa44d0cb4b87102da340cbc7eed31cc4124
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-05-18 16:09:49 +0200

    Add project files.

---
+++ Encapsulation/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortPerson
+{
+   public class Person
+    {
+        //private string firstName;
+        //private string lastName;
+        //private int age;
+        public Person()
+        {
+            Console.WriteLine("person added");
+        }
+        public Person(string firstName,string lastName,int age) :this()
+        {
+            this.Age = age;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+
+        }
+        
+        public string FirstName { get; set; }
+        public string LastName { get; private set; }
+            public int Age { get; set; }
+   public override string ToString()=> $"{ this.FirstName} { this.LastName } is { this.Age} years old";
+        
+    }
+   
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SortPerson;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = new List<Person>();
+            for(int i = 0; i < 5;i++)
+            {
+                var cmdArge = Console.ReadLine().Split();
+                var person = new Person(cmdArge[0], cmdArge[1], int.Parse(cmdArge[2]));
+                persons.Add(person);
+            }
+            persons.OrderBy(p => p.FirstName).ThenBy(p => p.Age).ToList().ForEach(p=>Console.WriteLine(p.ToString()));
+        }
+    }
+}
